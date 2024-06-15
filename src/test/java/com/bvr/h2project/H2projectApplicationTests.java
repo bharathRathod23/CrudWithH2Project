43e643c29fd0a2bd99796f715e284e8f0cc07dd5
bharath.vittal <bharath.vittal@ril.com>
2024-06-15 13:49:51 +0530

    added test cases

--- src/test/java/com/bvr/h2project/H2projectApplicationTests.java
+++ src/test/java/com/bvr/h2project/H2projectApplicationTests.java
@@ -1,13 +1,100 @@
 package com.bvr.h2project;
 
+import com.bvr.h2project.entity.Book;
+import com.bvr.h2project.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class H2projectApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testCreateBook() throws Exception {
+		mockMvc.perform(post("/api/books")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"title\": \"Test Book\", \"author\": \"Test Author\"}"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title", is("Test Book")))
+				.andExpect(jsonPath("$.author", is("Test Author")));
+	}
+
+
+	@Test
+	void testGetAllBooks() throws Exception {
+		Book book = new Book();
+		book.setTitle("Test Book");
+		book.setAuthor("Test Author");
+		bookRepository.save(book);
+
+		mockMvc.perform(get("/api/books"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].title", is("Test Book")))
+				.andExpect(jsonPath("$[0].author", is("Test Author")));
+	}
+
+	@Test
+	void testGetBookById() throws Exception {
+		Book book = new Book();
+		book.setTitle("Test Book");
+		book.setAuthor("Test Author");
+		book = bookRepository.save(book);
+
+		mockMvc.perform(get("/api/books/" + book.getId()))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title", is("Test Book")))
+				.andExpect(jsonPath("$.author", is("Test Author")));
+	}
+
+	@Test
+	void testUpdateBook() throws Exception {
+		Book book = new Book();
+		book.setTitle("Old Title");
+		book.setAuthor("Old Author");
+		book = bookRepository.save(book);
+
+		mockMvc.perform(put("/api/books/" + book.getId())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"title\": \"New Title\", \"author\": \"New Author\"}"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title", is("New Title")))
+				.andExpect(jsonPath("$.author", is("New Author")));
+	}
+
+	@Test
+	void testDeleteBook() throws Exception {
+		Book book = new Book();
+		book.setTitle("Test Book");
+		book.setAuthor("Test Author");
+		book = bookRepository.save(book);
+
+		mockMvc.perform(delete("/api/books/" + book.getId()))
+				.andExpect(status().isNoContent());
+
+		Optional<Book> deletedBook = bookRepository.findById(book.getId());
+		assert !deletedBook.isPresent();
+	}
+
 
 }
